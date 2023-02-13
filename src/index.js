9b141a1294bfceab867ed388a4a80808a7452db9
jeassons <jeassonsuarez@outlook.es>
2023-02-13 10:20:44 -0500

    probando 4

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import tarea from '../src/routes/tarea.routes.js'
 import usuario from '../src/routes/usuario.routes.js'
 import { PORT } from './config.js';
+import pool from './mysql.js';
+
 
 const app = express();
 app.use(express.json({limit: '50mb'}));
@@ -10,8 +12,9 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cors());
 
-app.get('/', (req, res)=>{
-    res.json({hola:'Saludo informal'})
+app.get('/', async (req, res)=>{
+    
+    res.json(await pool.query('select * from usuario'))
 })
 app.use(usuario)
 app.use(tarea)
