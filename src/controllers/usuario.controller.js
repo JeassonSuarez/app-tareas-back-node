281ce46490fc25fd9891f9582cc799643a176e3d
jeassons <jeassonsuarez@outlook.es>
2023-02-13 10:10:27 -0500

    probando

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -5,7 +5,8 @@ export const creaUsuario = async (req, res) =>{
 }
 
 export const traeUsuario = async (req, res) => {
-    const b = req.body;
-    const respuesta  = await pool.query(`select id, nusuario from usuario where nusuario="${b.nusuario}" and pass="${b.pass}"`)
-    respuesta.length===1 ? res.json({auth:true, id:respuesta[0], mensaje:'Se ha logueado'}) : res.json({auth:false, id:null, mensaje:'No se ha podido loguear'});
+    res.send('ook')
+    // const b = req.body;
+    // const respuesta  = await pool.query(`select id, nusuario from usuario where nusuario="${b.nusuario}" and pass="${b.pass}"`)
+    // respuesta.length===1 ? res.json({auth:true, id:respuesta[0], mensaje:'Se ha logueado'}) : res.json({auth:false, id:null, mensaje:'No se ha podido loguear'});
 }
